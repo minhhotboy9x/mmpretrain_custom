02b5b7c6651d4560d571dd01d740e5d00968353b
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2024-11-24 22:53:17 +0700

    gensmote

--- GenSMOTE/module.py
+++ GenSMOTE/module.py
@@ -6,6 +6,9 @@ import torch.nn as nn
 class Encoder(nn.Module):
     def __init__(self, args):
         super(Encoder, self).__init__()
+
+        self.embed = nn.Embedding(args['num_class'], args['dim_h'])
+
         self.conv = nn.Sequential(
             nn.Conv2d(args['n_channel'], args['dim_h'], 4, 2, 1, bias=False),  # 224 -> 112
             nn.LeakyReLU(0.2, inplace=True),
@@ -25,11 +28,12 @@ class Encoder(nn.Module):
             nn.BatchNorm2d(args['dim_h'] * 32),
             nn.LeakyReLU(0.2, inplace=True)
         )
-        self.fc = nn.Linear(args['dim_h'] * 32, args['n_z'])
+        self.fc = nn.Linear(args['dim_h'] * 33, args['n_z'])
 
-    def forward(self, x):
+    def forward(self, x, labels):
         x = self.conv(x)
         x = x.view(x.shape[0], -1)   # Shape: (batch_size, dim_h * 32)
+        x = torch.cat((x, self.embed(labels)), dim=1)  # Shape: (batch_size, dim_h * 33)
         x = self.fc(x)  # Shape: (batch_size, n_z)
         return x
 
@@ -37,8 +41,10 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, args):
         super(Decoder, self).__init__()
+
+        self.embed = nn.Embedding(args['num_class'], args['dim_h'])
         self.fc = nn.Sequential(
-            nn.Linear(args['n_z'], args['dim_h'] * 32),
+            nn.Linear(args['n_z'] + args['dim_h'], args['dim_h'] * 32),
             nn.ReLU()
         )
         self.deconv = nn.Sequential(
@@ -62,31 +68,34 @@ class Decoder(nn.Module):
             nn.Tanh()
         )
 
-    def forward(self, x):
-        x = self.fc(x).view(-1, args['dim_h'] * 32, 1, 1)  # Shape: (batch_size, dim_h * 32, 1, 1)
+    def forward(self, x, labels):
+        x = torch.cat((x, self.embed(labels)), dim=1)  # Shape: (batch_size, n_z + dim_h)
+        x = self.fc(x)  # Shape: (batch_size, dim_h * 32)
+        x = x.view(-1, args['dim_h'] * 32, 1, 1)  # Shape: (batch_size, dim_h * 32, 1, 1)
         x = self.deconv(x)  # Shape: (batch_size, 1, 224, 224)
         return x
 
-class DeepSMOTE(nn.Module):
+class GenSMOTE(nn.Module):
     def __init__(self, args):
-        super(DeepSMOTE, self).__init__()
+        super(GenSMOTE, self).__init__()
         self.encoder = Encoder(args)
         self.decoder = Decoder(args)
 
-    def encode(self, x):
-        return self.encoder(x)
+    def encode(self, x, labels):
+        return self.encoder(x, labels)
     
-    def decode(self, z):
-        return self.decoder(z)
+    def decode(self, z, labels):
+        return self.decoder(z, labels)
 
-    def forward(self, x):
-        z = self.encoder(x)
-        x_recon = self.decoder(z)
+    def forward(self, x, labels):
+        z = self.encoder(x, labels)
+        x_recon = self.decoder(z, labels)
         return x_recon
     
 
 # Example arguments
 args = {
+    'num_class': 7,
     'dim_h': 64,
     'n_channel': 3,
     'n_z': 256
@@ -99,7 +108,8 @@ if __name__ == '__main__':
     encoder.eval()
     decoder.eval()
     input = torch.randn(1, 3, 224, 224)
-    z = encoder(input)
+    labels = torch.tensor([1])
+    z = encoder(input, labels)
     print(z.shape)  # torch.Size([1, 256])
-    output = decoder(z)
+    output = decoder(z, labels)
     print(output.shape)  # torch.Size([1, 1, 224, 224])
--- GenSMOTE/train.py
+++ GenSMOTE/train.py
@@ -9,7 +9,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 from torchvision.utils import save_image
 from tqdm import tqdm
-from module import DeepSMOTE
+from module import GenSMOTE
 from sklearn.neighbors import NearestNeighbors
 from dataloader import build_dataloader
 
@@ -17,15 +17,15 @@ from dataloader import build_dataloader
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
-train_annotations_file = "dataset/final_malaria_full_class_classification/train_annotation.txt"
-train_img_dir = "dataset/final_malaria_full_class_classification"
+# train_annotations_file = "dataset/final_malaria_full_class_classification/train_annotation.txt"
+# train_img_dir = "dataset/final_malaria_full_class_classification"
 
-val_annotations_file = "dataset/final_malaria_full_class_classification/val_annotation.txt"
-val_img_dir = "dataset/final_malaria_full_class_classification"
+# val_annotations_file = "dataset/final_malaria_full_class_classification/val_annotation.txt"
+# val_img_dir = "dataset/final_malaria_full_class_classification"
 
 batch_size = 4
 epochs = 100
-lr0 = 1e-3
+lr0 = 1e-2
 lrf = 1e-2
 momentum = 0.937 # (float) SGD momentum/Adam beta1
 weight_decay = 0.0005 # (float) optimizer weight decay 5e-4
@@ -33,7 +33,8 @@ num_workers = 4
 args = {
         'dim_h': 64,
         'n_channel': 3,
-        'n_z': 256
+        'n_z': 256,
+        'num_class': 7,
     } 
 
 
@@ -43,34 +44,35 @@ transform = transforms.Compose([
     transforms.Resize((224, 224)),           # Thay đổi kích thước ảnh
     transforms.RandomHorizontalFlip(p=0.5), # Lật ngang với xác suất 50%
     transforms.RandomVerticalFlip(p=0.5),   # Lật dọc với xác suất 50%
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Thay đổi màu sắc
     transforms.ToTensor()                   # Chuyển ảnh thành Tensor
 ])
 
   
 # ----------------- DataLoader ----------------- #
-train_loader = build_dataloader(train_annotations_file,
-                                train_img_dir,
-                                transform, 
-                                batch_size=batch_size, 
-                                minor_classes=[0, 1, 2, 3],
-                                remove_major=True,
-                                num_workers=num_workers,
-                                shuffle=True)
+# train_loader = build_dataloader(train_annotations_file,
+#                                 train_img_dir,
+#                                 transform, 
+#                                 batch_size=batch_size, 
+#                                 minor_classes=[0, 1, 2, 3],
+#                                 remove_major=True,
+#                                 num_workers=num_workers,
+#                                 shuffle=True)
 
-val_loader = build_dataloader(val_annotations_file,
-                                val_img_dir,
-                                transform=transform,
-                                batch_size=batch_size, 
-                                minor_classes=[0, 1, 2, 3],
-                                remove_major=True,
-                                num_workers=num_workers,
-                                shuffle=False)
+# val_loader = build_dataloader(val_annotations_file,
+#                                 val_img_dir,
+#                                 transform=transform,
+#                                 batch_size=batch_size, 
+#                                 minor_classes=[0, 1, 2, 3],
+#                                 remove_major=True,
+#                                 num_workers=num_workers,
+#                                 shuffle=False)
 
 # ----------------- Criterion ----------------- #
 criterion = nn.MSELoss()
 
 # ----------------- Model ----------------- #
-model = DeepSMOTE(args)
+model = GenSMOTE(args)
 
 # ----------------- Optimizer ----------------- #
 
@@ -78,7 +80,7 @@ optimizer = optim.SGD(model.parameters(), lr=lr0, momentum=momentum, weight_deca
 scheduler = CosineAnnealingLR(optimizer, T_max=epochs, eta_min=lr0*lrf)
 
 # ----------------- Training ----------------- #
-def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, device, log_dir="work_dirs/deepsmote_image_logs"):
+def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, device, log_dir="work_dirs/gensmote_image_logs"):
     
     # Khởi tạo TensorBoard SummaryWriter
     writer = SummaryWriter(log_dir=log_dir)
@@ -93,7 +95,6 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
         model.train()
         running_loss = 0.0
         running_R_loss = 0.0
-        running_P_loss = 0.0
         
         # Vòng lặp huấn luyện
         train_loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{num_epochs}]")
@@ -103,21 +104,10 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
             # Sử dụng AMP để huấn luyện
             with amp.autocast(device_type='cuda'): 
                 # Reconstruction loss
-                outputs = model(images)
+                outputs = model(images, labels)
                 R_loss = criterion(outputs, images)
+                loss = R_loss
                 
-                # Penalty loss
-                minor_images, minor_labels = train_loader.dataset.sample_minor_img(train_loader.batch_size)
-                minor_images = minor_images.to(device)  # Đưa minor_images lên thiết bị
-                e_minor = model.encode(minor_images)
-                shuffled_indices = torch.randperm(e_minor.size(0))  # Sinh thứ tự ngẫu nhiên cho b
-                e_shuffled_minor = e_minor[shuffled_indices]
-                d_shuffled_minor = model.decode(e_shuffled_minor)
-                P_loss = criterion(d_shuffled_minor, minor_images)
-
-                # Total loss
-                loss = R_loss + P_loss
-            
             # Backward và cập nhật optimizer
             optimizer.zero_grad()
             
@@ -129,7 +119,6 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
             # Cộng dồn loss để tính trung bình
             running_loss += loss.item()
             running_R_loss += R_loss.item()
-            running_P_loss += P_loss.item()
             
             # Log loss vào TensorBoard mỗi bước
             # writer.add_scalar('Train/Total_Loss', loss.item(), epoch * len(train_loader) + step)
@@ -139,14 +128,10 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
             # Cập nhật progress bar
             train_loop.set_postfix({
                 "Total_Loss": loss.item(),
-                "R_Loss": R_loss.item(),
-                "P_Loss": P_loss.item()
             })
 
         # Log loss trung bình mỗi epoch
-        writer.add_scalar('Epoch/Total_Loss', running_loss / len(train_loader), epoch)
-        writer.add_scalar('Epoch/Reconstruction_Loss', running_R_loss / len(train_loader), epoch)
-        writer.add_scalar('Epoch/Penalty_Loss', running_P_loss / len(train_loader), epoch)
+        writer.add_scalar('Train/Total_Loss', running_loss / len(train_loader), epoch)
 
         # validation
         val_loss = 0.0
@@ -155,7 +140,8 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
             with torch.no_grad():
                 model.eval()
                 images = images.to(device)
-                gen_images = model(images)
+                labels = labels.to(device)
+                gen_images = model(images, labels)
                 val_loss += criterion(gen_images, images)
             
             save_image(images[0].cpu(), os.path.join(log_dir, f"original_batch_{0}.png"))
@@ -167,6 +153,8 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
             best_loss = val_loss
             torch.save(model.state_dict(), os.path.join(log_dir, 'best.pt'))  # Lưu mô hình tốt nhất
 
+        writer.add_scalar('Val/Total_Loss', val_loss / len(val_loader), epoch)
+
         # Lưu mô hình cuối cùng sau mỗi epoch
         torch.save(model.state_dict(), os.path.join(log_dir, 'last.pt'))  # Lưu mô hình cuối cùng
 
@@ -174,26 +162,29 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs, dev
     writer.close()
 
 if __name__ == '__main__':
-    # train_annotations_file = "dataset/final_malaria_full_class_classification/train_annotation.txt"
-    # train_img_dir = "dataset/final_malaria_full_class_classification"
+    train_annotations_file = "dataset/final_malaria_full_class_classification/train_annotation.txt"
+    train_img_dir = "dataset/final_malaria_full_class_classification"
 
-    # val_annotations_file = "dataset/final_malaria_full_class_classification/val_annotation.txt"
-    # val_img_dir = "dataset/final_malaria_full_class_classification"
+    val_annotations_file = "dataset/final_malaria_full_class_classification/val_annotation.txt"
+    val_img_dir = "dataset/final_malaria_full_class_classification"
 
-    # train_loader = build_dataloader(train_annotations_file,
-    #                             train_img_dir,
-    #                             transform, 
-    #                             batch_size=batch_size, 
-    #                             minor_classes=[0, 1, 2, 3], 
-    #                             num_workers=num_workers)
+    train_loader = build_dataloader(train_annotations_file,
+                                    train_img_dir,
+                                    transform, 
+                                    batch_size=batch_size, 
+                                    minor_classes=[0, 1, 2, 3],
+                                    remove_major=True,
+                                    num_workers=num_workers,
+                                    shuffle=True)
 
-    # val_loader = build_dataloader(val_annotations_file,
-    #                                 val_img_dir,
-    #                                 transform=transform,
-    #                                 batch_size=batch_size, 
-    #                                 minor_classes=[0, 1, 2, 3],
-    #                                 num_workers=num_workers,
-    #                                 shuffle=False)
+    val_loader = build_dataloader(val_annotations_file,
+                                    val_img_dir,
+                                    transform=transform,
+                                    batch_size=batch_size, 
+                                    minor_classes=[0, 1, 2, 3],
+                                    remove_major=True,
+                                    num_workers=num_workers,
+                                    shuffle=False)
 
     # Initialize models
     train(model, train_loader, val_loader, criterion, optimizer, epochs, device)
